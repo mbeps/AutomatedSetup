b6b03a5be3cb3f01868a81f3b08988eded192530
Maruf Bepary <58662575+mbeps@users.noreply.github.com>
2022-03-26 18:29:37 +0000

    Enable full Flathub repositories

--- Fedora.sh
+++ Fedora.sh
@@ -13,3 +13,8 @@ function enable_rpm_fusion() {
 	sudo dnf install \ https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm # Free
 	sudo dnf install \ https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm # Non Free
 }
+
+function enable_flathub() {
+    title "Enable FlatHub"
+    flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+}
