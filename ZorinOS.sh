20d3c44535ee025fd948d603c06a3de0290d3c11
Maruf Bepary <58662575+mbeps@users.noreply.github.com>
2022-03-26 16:41:51 +0000

    Installing Nala APT front end - Tested

--- ZorinOS.sh
+++ ZorinOS.sh
@@ -46,3 +46,10 @@ function installPostgres() {
 	title "Installing PostgreSQL"
 	sudo apt install postgresql postgresql-contrib -y
 }
+
+function installNala() {
+	title "Install Nala Front End for APT"
+	echo "deb [arch=amd64,arm64,armhf] http://deb.volian.org/volian/ scar main" | sudo tee /etc/apt/sources.list.d/volian-archive-scar-unstable.list
+	wget -qO - https://deb.volian.org/volian/scar.key | sudo tee /etc/apt/trusted.gpg.d/volian-archive-scar-unstable.gpg > /dev/null
+	sudo apt install nala -y
+}
