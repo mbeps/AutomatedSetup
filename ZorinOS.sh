08bb320a6d6027432eeb226aa38be586b11e5fbe
Maruf Bepary <58662575+mbeps@users.noreply.github.com>
2022-03-26 16:39:55 +0000

    Installing JDK - Tested - Amazon Corretto 17

--- ZorinOS.sh
+++ ZorinOS.sh
@@ -23,3 +23,11 @@ function installMediaCodecs() {
 	sudo add-apt-repository multiverse -y
 	sudo apt install ubuntu-restricted-extras -y
 }
+
+#^ DEVELOOMENT
+function installJDK() {
+	title "Install Java Development Environment"
+	 wget -O- https://apt.corretto.aws/corretto.key | sudo apt-key add - 
+ sudo add-apt-repository 'deb https://apt.corretto.aws stable main'
+  sudo apt-get update; sudo apt-get install -y java-17-amazon-corretto-jdk
+}
