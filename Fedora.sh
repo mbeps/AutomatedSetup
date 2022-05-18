a958c2ac0127c99779183a23e468490fd38b7a1c
Maruf Bepary <bepary71@gmail.com>
2022-05-18 18:20:08 +0100

    Updated older Fedora 35 commands to Fedora 36 - Installing Postgres - Enabling RPM Fusion - Installing media codecs - Installing JDK 17 LTS - Removed obsolete install and remove commands

--- Fedora.sh
+++ Fedora.sh
@@ -6,25 +6,29 @@ function title() {
 function updateAndUpgrade() {
 	title "Update & Upgrade"
 	sudo dnf update -y
-    sudo dnf upgrade -y
+	sudo dnf upgrade -y
 }
 
 function optimiseDNF() {
-    title "Opmise DNF"
-    echo 'fastestmirror=1' | sudo tee -a /etc/dnf/dnf.conf
-    echo 'max_parallel_downloads=10' | sudo tee -a /etc/dnf/dnf.conf
+	title "Opmise DNF"
+	echo 'fastestmirror=1' | sudo tee -a /etc/dnf/dnf.conf
+	echo 'max_parallel_downloads=10' | sudo tee -a /etc/dnf/dnf.conf
 
 }
 
 function enableRPMFusion() {
 	title "Enable RPM Fusion Respositories"
-	sudo dnf install \ https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm # Free
-	sudo dnf install \ https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm # Non Free
+	sudo dnf install \
+		https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+	# Free
+	sudo dnf install \
+		https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+	# Non Free
 }
 
 function enableFlathub() {
-    title "Enable FlatHub"
-    flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+	title "Enable FlatHub"
+	flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 }
 
 function installMicrosoftFonts() {
@@ -35,17 +39,17 @@ function installMicrosoftFonts() {
 
 function installMediaCodecs() {
 	title "Installing Media CODECs"
-	sudo dnf install gstreamer1-plugins-{bad-\*,good-\*,base} gstreamer1-plugin-openh264 gstreamer1-libav --exclude=gstreamer1-plugins-bad-free-devel
-	sudo dnf install lame\* --exclude=lame-devel
+	sudo dnf install gstreamer1-plugins-{bad-\*,good-\*,base} gstreamer1-plugin-openh264 gstreamer1-libav --exclude=gstreamer1-plugins-bad-free-devel -y
+	sudo dnf install lame\* --exclude=lame-devel -y
 	sudo dnf group upgrade --with-optional Multimedia
 }
 
 #^ DEVELOPMENT
 function installJDK() {
-    title "Instaling Java Development Environment"
-    sudo rpm --import https://yum.corretto.aws/corretto.key 
-    sudo curl -L -o /etc/yum.repos.d/corretto.repo https://yum.corretto.aws/corretto.repo
-    sudo yum install -y java-17-amazon-corretto-devel
+	title "Instaling Java Development Environment"
+	sudo rpm --import https://yum.corretto.aws/corretto.key -y
+	sudo curl -L -o /etc/yum.repos.d/corretto.repo https://yum.corretto.aws/corretto.repo
+	sudo yum install -y java-17-amazon-corretto-devel
 }
 
 function installAndSetupGit() {
@@ -60,18 +64,8 @@ function installAndSetupGit() {
 
 function installPostgres() {
 	title "Installing PostgreSQL"
-	sudo dnf install postgresql-server postgresql-contrib -y
-    sudo systemctl enable postgresql
-    sudo postgresql-setup --initdb --unit postgresql
-    sudo systemctl start postgresql
-}
-
-function installNativeApps() {
-    title "Installing Natively Packaged Apps"
-    sudo dnf install gnome-tweak-tool -y
-}
-
-function removeApps() {
-    title "Remove Unnecessary Native Apps"
-    sudo dnf remove gedit -y
+	sudo dnf install postgresql-server postgresql-contrib
+	sudo systemctl enable postgresql
+	sudo postgresql-setup --initdb --unit postgresql
+	sudo systemctl start postgresql
 }
