0b84559f3e1e27e6a99abb44196a6cbbf5bd4807
Maruf Bepary <bepary71@gmail.com>
2022-06-26 11:42:32 +0100

    Created font installer - Installs Cascadia Code - Installs Windows proprietary fonts

---
+++ fonts.sh
@@ -0,0 +1,19 @@
+# Contains funtions to install various fonts.
+
+source "./dependencies.sh"
+
+# Install Cascadia Code Font from RPM repository. 
+function installCascadiaCodeFont() {
+	title "Installing Cascadia Code Font"
+
+	package=("cascadia-fonts-all")
+	installNativeApps "${package[@]}"
+}
+
+# Installs Microsoft Windows Proprietary fonts. 
+	# Copies fonts into the fonts folder. 
+function installWindowsFonts() {
+	title "Installing Windows Proprietary Fonts"
+
+	cp resources/fonts/windows /usr/share/fonts
+}
