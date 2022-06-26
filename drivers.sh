70617e9f46e4e6977aeca16ddc905164aac971a3
Maruf Bepary <bepary71@gmail.com>
2022-06-26 12:06:02 +0100

    Install drivers and CODECs

---
+++ drivers.sh
@@ -0,0 +1,27 @@
+# Contains funtions to install drivers and CODECs.
+
+source "./dependencies.sh"
+
+# Installs required drivers which are not shipped by default. 
+	# Installs media drivers
+	# Source: https://www.reddit.com/r/intel/comments/l22le8/what_is_the_intelmediadriver/
+function installDrivers() {
+	title "Installing Required Drivers"
+
+	drivers=(
+		"libva-intel-driver"
+		"intel-media-driver"
+		"gstreamer1-vaapi"
+	)
+	installNativeApps "${drivers[@]}"
+}
+
+# Installs media codecs for added video support and video playback performance. 
+	# Source: https://ostechnix.com/how-to-install-multimedia-codecs-in-fedora-linux/
+function installMediaCodecs() { 
+	title "Installing Media CODECs"
+
+	sudo dnf install gstreamer1-plugins-{bad-\*,good-\*,base} gstreamer1-plugin-openh264 gstreamer1-libav --exclude=gstreamer1-plugins-bad-free-devel -y
+	sudo dnf install lame\* --exclude=lame-devel -y
+	sudo dnf group upgrade --with-optional Multimedia -y
+}
