18362764a787448a965b04aec5a73e3a092c8f6a
Maruf Bepary <bepary71@gmail.com>
2022-06-26 11:31:54 +0100

    Created dependencies files - Contains important funtions

---
+++ dependencies.sh
@@ -0,0 +1,40 @@
+# This file contains resources (functions and variables) which are critical to other files' execution. 
+
+# Displays title for functions and clearns previous output. 
+	# Args:
+		# - $1: title to be printed. 
+function title() {
+	clear
+	echo $1
+}
+
+#^ DEPENDENCIES
+# Installs FlatHub apps from a list. 
+	# Args:
+		# - $1: array of apps
+function installFlathubApps() {
+	apps=$1
+	for app in "${apps[@]}"; do
+		flatpak install flathub $app -y
+	done
+}
+
+# Installs native apps and packages from a list. 
+	# Args:
+		# - $1: array of packages
+function installNativeApps() {
+	packages=$1
+	for package in "${packages[@]}"; do
+		sudo dnf install $package -y
+	done
+}
+
+# Removes native apps and packages from a list. 
+	# Args:
+		# - $1: array of apps
+function removeNativeApps() {
+	packages=$1
+	for package in "${packages[@]}"; do
+		sudo dnf remove $package -y
+	done
+}
