d2f3783bcdbc64e4f970caf996c91f11eeb7f02b
Maruf Bepary <bepary71@gmail.com>
2022-07-24 20:03:28 +0600

    Cleaned - Removed unnecessary titles and spaces

--- modules/archive.sh
+++ modules/archive.sh
@@ -7,8 +7,6 @@
 		# - $2: name and location where splitted archive is to be saved
 	# Source: https://superuser.com/questions/336219/how-do-i-split-a-zip-file-into-multiple-segments#:~:text=Add%20a%20comment-,3%20Answers,-Sorted%20by%3A
 function split_archive() { original_archive=$1; new_decomposed_archive=$2
-	title "Splitting Archive"
-
 	zip "$original_archive" --out "$new_decomposed_archive" -s 90m
 }
 
@@ -18,8 +16,6 @@ function split_archive() { original_archive=$1; new_decomposed_archive=$2
 		# - $2: name and location of archive to be reconstructed
 	# Source: https://superuser.com/questions/336219/how-do-i-split-a-zip-file-into-multiple-segments#:~:text=Add%20a%20comment-,3%20Answers,-Sorted%20by%3A
 function reconstruct_archive() { decomposed_archive=$1; reconstruct_archive=$2
-	title "Recontructing Archive"    
-
 	zip -F "$decomposed_archive" --out "$reconstruct_archive"
 }
 
@@ -29,8 +25,6 @@ function reconstruct_archive() { decomposed_archive=$1; reconstruct_archive=$2
 		# - $2: name and location of target to be archived
 	# Source: https://superuser.com/questions/336219/how-do-i-split-a-zip-file-into-multiple-segments#:~:text=Add%20a%20comment-,3%20Answers,-Sorted%20by%3A
 function compress_archive() { archive_name=$1; target=$2
-	title "Compress Archive"
-
 	zip -r "$archive_name" "$target"
 }
 
@@ -40,8 +34,6 @@ function compress_archive() { archive_name=$1; target=$2
 		# - $2: location where archive is to be uncompressed to
 	# Source: https://superuser.com/questions/336219/how-do-i-split-a-zip-file-into-multiple-segments#:~:text=Add%20a%20comment-,3%20Answers,-Sorted%20by%3A 
 function uncompress_archive() {
-	title "Uncompress Archive"
-
 	archive=$1
 	destination=$2
 
