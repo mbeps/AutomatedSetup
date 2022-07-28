164f94daebf68457eea7c1edc6af80a6d1af4f56
Maruf Bepary <bepary71@gmail.com>
2022-07-28 18:43:13 +0600

    Added array - Array of backups - Array of originals

--- modules/configuration.sh
+++ modules/configuration.sh
@@ -27,3 +27,35 @@ function restore_configuration() { backup=$1; original=$2
 function reset_configuration() { target=$1
 	dconf reset -f "$target"
 }
+
+# Backs up dconf configurations from list. 
+	# Args:
+		# - $1: name and location configuration to be backed up
+		# - $2: name and location of backup file
+	# Source: https://www.reddit.com/r/gnome/comments/l3ha66/how_to_export_and_import_all_gnome_keyboard/?utm_source=share&utm_medium=web2x&context=3
+function backup_configurations() { backups=$1; originals=$2
+	backups_length=${#backups[@]}
+	originals_length=${#originals[@]}
+
+	if [[ backups_length -eq originals_length]]
+	then
+		for i in "${!backups[@]}"; do
+  			backup_configuration "${backups[$i]}" "${originals[$i]}"
+	fi
+}
+
+# Restores dconf configurations from list.  
+	# Args:
+		# - $1: list of names and locations of backed up configurations 
+		# - $2: list of names and locations of configuration to be restored
+	# Source: https://www.reddit.com/r/gnome/comments/l3ha66/how_to_export_and_import_all_gnome_keyboard/?utm_source=share&utm_medium=web2x&context=3
+function restore_configurations() { originals=$1; backups=$2
+	backups_length=${#backups[@]}
+	originals_length=${#originals[@]}
+
+	if [[ backups_length -eq originals_length]]
+	then
+		for i in "${!backups[@]}"; do
+  			restore_configuration "${originals[$i]}" "${backups[$i]}" 
+	fi
+}
