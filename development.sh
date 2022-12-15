91e895b6895a91476c05372743df94b2c629db83
Maruf Bepary <bepary71@gmail.com>
2022-12-15 13:45:03 +0000

    Updates: - Install MySQL - Update VSCode installation - Update flatpak apps

--- development.sh
+++ development.sh
@@ -62,6 +62,15 @@ function install_postgres() {
 	sudo systemctl start postgresql
 }
 
+# Installs MySQL database
+	# Source_ https://docs.fedoraproject.org/en-US/quick-docs/installing-mysql-mariadb/
+function install_mysql() { 
+	title "Installing MySQL"
+
+	package=("community-mysql-server")
+	install_native_apps "${package[@]}"
+}
+
 # Installs Pip package manager for Python. 
 	# Source: https://www.osradar.com/install-pip-fedora-34/
 function install_python_pip() {
@@ -86,18 +95,8 @@ function install_vscode() {
 	# https://www.linuxcapable.com/how-to-install-visual-studio-code-vs-code-on-fedora-34-35/
 	title "Installing Visual Studio Code"
 
-	respository=(
-		"[vscode]"
-		"name=Visual Studio Code"
-		"baseurl=https://packages.microsoft.com/yumrepos/vscode"
-		"enabled=1"
-		"gpgcheck=1"
-		"gpgkey=https://packages.microsoft.com/keys/microsoft.asc"
-	)
-	for line in "${respository[@]}"; do # Imports repository
-		echo "$line" | sudo tee -a /etc/yum.repos.d/vscode.repo
-	done
-	sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc # Import GPG key
+	sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+	sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 	
 	package=("code")
 	install_native_apps "${package[@]}"
@@ -140,21 +139,21 @@ function install_docker() {
 	sudo dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
 	packages=(
 		"docker-ce" 
-		"docker-ce-cli "
+		"docker-ce-cli"
 		"containerd.io"
-		"docker-desktop-4.11.0-x86_64.rpm"
 	)
 	install_native_apps "${packages[@]}"
 }
 
 # Installs FlatHub apps required for development. 
 function install_flathub_apps_development() { 
-	title "Installing Non-System Flathub Apps"
+	title "Installing Development Flathub Apps"
 	
 	apps=(
 		"io.dbeaver.DBeaverCommunity" 			# DBeaver - Database App
 		"rest.insomnia.Insomnia" 				# Insomnia - API Tool
 		"org.gnome.gitg" 						# Git GUI
+		"org.gnome.Boxes"						# Boxes - Virtual Machines
 		)
 
 	install_flathub_apps "${apps[@]}"
