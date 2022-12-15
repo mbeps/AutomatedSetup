b48ad0c8c4a6cbb69ff5ebf90ec38586928c5677
Maruf Bepary <bepary71@gmail.com>
2022-12-15 12:43:06 +0000

    Added Gnome configurations

--- gnome_configuration.sh
+++ gnome_configuration.sh
@@ -24,4 +24,18 @@ function gnome_fractional_scaling() {
 	# gsettings reset org.gnome.mutter experimental-features 								# Reset
 }
 
+# Backs up general Gnome configurations. 
+function backup_general_configuration() { 
+	dconf dump /org/gnome/mutter/draggable-border-width/ >> ./resources/general-gnome-settings/draggable-border-width
+	dconf dump /org/gnome/terminal/legacy/new-terminal-mode/ >> ./resources/general-gnome-settings/new-terminal-mode
+	dconf dump /org/gnome/software/packaging-format-preference/ >> ./resources/general-gnome-settings/packaging-format-preference
+}
+
+# Restores general Gnome configurations from dconf files. 
+function restore_general_configuration() { 
+	cat ./resources/general-gnome-settings/draggable-border-width | dconf load /org/gnome/mutter/draggable-border-width/
+	cat ./resources/general-gnome-settings/new-terminal-mode | dconf load /org/gnome/terminal/legacy/new-terminal-mode/
+	cat ./resources/general-gnome-settings/ | dconf load /org/gnome/software/packaging-format-preference/
+}
+
 "$@"
