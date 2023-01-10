6776b01fca693dc7c64f2c3bece962de590e3962
Maruf Bepary <bepary71@gmail.com>
2023-01-10 17:30:49 +0000

    Added missing installs - Edge - Insync

--- main.sh
+++ main.sh
@@ -24,6 +24,8 @@ function automated_normal() {
 	install_flathub_apps_non_system
 	install_flathub_apps_system
 	remove_preinstalled_flatpaks
+	install_insync
+	install_microsoft_edge
 
 	restore_extensions
 	restore_extension_configuration
